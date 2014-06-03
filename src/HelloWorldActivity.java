3a5717da307fdcdbf2595be0f3787c426af1a282
Daniel Ramirez <javamonn@gmail.com>
2014-06-02 22:53:59 -0500

    updated to immersion

---
+++ src/HelloWorldActivity.java
@@ -0,0 +1,19 @@
+import com.google.android.glass.app.Card;
+import com.google.android.glass.timeline.LiveCard;
+
+import android.app.Activity;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.IBinder;
+
+
+public class HelloWorldActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		Card helloWorld = new Card(this);
+		helloWorld.setText("Hello, World!");
+		helloWorld.setFootnote("Google Glass Crash Course");
+		setContentView(helloWorld.getView());
+	}
+}
